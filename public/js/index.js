be771c7bf2a29f1a8026fd15c885feed3537f6d9
Bilal Bhakhrani <bilal.bhakhrani@gmail.com>
2016-10-26 23:24:25 -0700

    Separate game logic from dom nodes to facilitate tree search

--- public/js/index.js
+++ public/js/index.js
@@ -1,115 +1,118 @@
+/**
+ * This file handles game play mechanics and  dom manipulations. minimax.js  is called from here to
+ * handle decision logic and is unaware of the dom. 
+ */
 // dom nodes
-let board = document.querySelector('.board');
-let squares = [...document.querySelectorAll('.square')];
-let length = Math.sqrt(squares.length);
-let rows = [...document.querySelectorAll('.row')]
-				.map(row => [...row.querySelectorAll('.square')]);
-let columns = computeColumns();
-let diagonals = computeDiagonals();
+const board = document.querySelector('.board');
+const squares = [...document.querySelectorAll('.square')];
+const length = Math.sqrt(squares.length);
 
 // state
 let player1 = true;
-let isWinner = false;
+let isGameInProgress = true;
 
-function computeColumns() {
+//constants
+const X = "X";
+const O = "O";
+const WINNING_GROUPS = getWinningGroups();
+
+/**
+ * Gets all possible sequences that can result in a win
+ */
+function getWinningGroups() {
+	let columnIndexes = getColumnIndexes();
+	let rowIndexes = getRowIndexes();
+	let diagonalIndexes = getDiagonalIndexes();
+	return [...columnIndexes, ...rowIndexes, ...diagonalIndexes];
+}
+
+function getColumnIndexes() {
 	let columns = [[],[],[]];
 	for(let i=0; i < squares.length; i++) {
 		let colIndex = i % length;
-		let current = squares[i];
-		columns[colIndex].push(current);
+		columns[colIndex].push(i);
 	}
 	return columns;
 }
 
-function computeDiagonals() {
+function getRowIndexes() {
+	let rows = [[],[],[]];
+	for (let i=0; i<squares.length; i++) {
+		let rowIndex = Math.floor(i/length);
+		rows[rowIndex].push(i);
+	}
+	return rows;
+}
+
+function getDiagonalIndexes() {
 	let diagonals = [[],[]];
 	let leftToRight = diagonals[0];
 	let rightToLeft = diagonals[1];
 
 	//left to right
 	for (let i=0; i < squares.length; i+=length + 1) {
-		leftToRight.push(squares[i]);
+		leftToRight.push(i);
 	} 
 
 	//right to left
 	let firstColumnOfLastRow = squares.length - length;
 	for (let i=length-1; i <= firstColumnOfLastRow; i+= length - 1) {
-		rightToLeft.push(squares[i]);
+		rightToLeft.push(i);
 	}
 	return diagonals;
 }
 
-function onBoardClick(event) {
-	if (isWinner) return;
-	fillSquare(event.target);
-	isWinner = checkWin();
-	!isWinner && checkTie();
+/**
+ * Converts state of board from DomNodes to a matrix for easier computation.
+ * A single square can be filled with ["", x, o]
+ * 
+ * @return {Matrix} 2D array representing the board
+ */
+function getState() {
+	return squares.map((square) =>  square.innerHTML);
 }
 
-function fillSquare(node) {
-	if (node.classList.contains('filled')) {
+function onBoardClick(event) {
+	if (!!event.target.innerHTML) return; //non empty square then do nothing
+	doTurn(event.target, X);
+	if (isGameInProgress) {
+		makeOpposingMove();
+	}
+}
+
+function doTurn(node, player) {
+	if (!isGameInProgress) return; // game is over, do nothing
+	
+	fillSquare(node, player);
+	
+	let state = getState();
+	let winningGroup = checkWin(state, player);
+	
+	if (winningGroup) {
+		console.log("winner", winningGroup);
+		isGameInProgress = false;
+		board.className += " win";
+		drawWin(winningGroup);
+	} else {
+		checkTie(state);
+	}
+}
+/**
+ * Start off by making random move, will later be filled with AI
+ */
+function makeOpposingMove() {
+	let moveIndex = pickOpposingMove(getState(), O);
+
+	doTurn(squares[moveIndex], O);
+}
+
+function fillSquare(node, player) {
+	if (node.classList.contains('filled')) { // do nothing if square is filled
 		return;
 	} else {
 		node.className += " filled";
-		node.innerHTML = (player1) ? "X" : "O";
+		node.innerHTML = player;
 		player1 = !player1;
-	}
-}
-
-/**
- * Checks if either player has won the game and shows victory
- * on baord if there is a winner.
- *
- * A win is when 3 squars line up in:
- * 	- one row
- * 		1,2,3
- * 		4,5,6
- * 		7,8,9
- * 	- one column
- * 		1,4,7
- * 		2,5,8
- * 		3,6,9
- * 	- diagonal (top left or top right)
- * 		1,5,9
- * 		3,5,7
- * 
- * @return {Boolean} true means game is over, false means continue
- */
-function checkWin() {
-	let winningGroup; 
-	winningGroup = checkGroups(rows);
-	if (!winningGroup) {
-		winningGroup = checkGroups(columns);
-	}
-	if (!winningGroup) {
-		winningGroup = checkGroups(diagonals);
-	}
-
-	if (winningGroup) {
-		console.log("winner", winningGroup);
-		board.className += " win";
-		drawWin(winningGroup);
-	}
-
-	return !!winningGroup;
-}
-
-function checkGroups(groups) {
-	for(let group of groups) {
-		let first = group[0].innerHTML;
-		let winner = group.every(square => square.innerHTML && square.innerHTML == first);
-		if (winner) {
-			return group;
-		}
-	}
-	return null;
-}
-
-function checkTie() {
-	let allFilled = squares.every(node => !!node.innerHTML);
-	if (allFilled) {
-		console.log("tie");
-		board.className += " tie";
 	}
 }
 
@@ -118,8 +121,8 @@ function checkTie() {
  * @param  {[type]} TODO [description]
  * @return {[type]}      [description]
  */
-function drawWin(nodes) {
-	nodes.forEach(node => node.className += " winner");
+function drawWin(indexes) {
+	indexes.forEach(index => squares[index].className += " winner");
 }
 
 function newGame() {
@@ -128,5 +131,93 @@ function newGame() {
 		square.innerHTML = "";
 		square.className = "square";
 		player1 = true;
+		isGameInProgress = true;
 	})
 }
+
+/**
+ * Predict a move with minmax algorithm:
+ * Given: Player X already made a move, what move should O make
+ *
+ * ===Sample===
+ * |x| | |
+ * | | | |
+ * | | | |
+ * X went first and picked top left corner (0,0)
+ * o has to pick next move
+ */
+
+/* 
+* A description for the algorithm, assuming X is the "turn taking player," would look something like:
+*
+* If the game is over, return the score from X's perspective.
+* Otherwise get a list of new game states for every possible move
+* Create a scores list
+* For each of these states add the minimax result of that state to the scores list
+* If it's X's turn, return the maximum score from the scores list
+* If it's O's turn, return the minimum score from the scores list
+* You'll notice that this algorithm is recursive, it flips back and forth between the players until a final score is found.
+*/
+
+/**
+ * Given the state of the board, computes the next move by delgating to 
+ * a selection algorithm. Expected to be called form index.js
+ * @param  {Array} state lis of DomNode that belong to the game grid
+ * @param {String} player the player we are making a move for
+ * @return {DomNode}         the DomNode that was selected to fill
+ */
+ function pickOpposingMove(state, O) {
+ 	return pickRandomSquare(state);
+ }
+
+/**
+ * Simple algorithm to pick a rando empty square
+ * 
+ * @param  {Array} state List of DomNode that belong to the game grid
+ * @return {DomNode}      the selected random dom node that is empty
+ */
+ function pickRandomSquare(state) {
+ 	//square is value of a square acutally, so only keeping empty squares aka where value is "" == false
+ 	let emptySquares = state.reduce((previous, current, currentIndex) => {
+ 		!current && previous.push(currentIndex);
+ 		return previous;
+ 	}, []);
+ 	let randomIndex = Math.random() * emptySquares.length
+ 	randomIndex = Math.floor(randomIndex);
+ 	return emptySquares[randomIndex];
+ }
+
+ /**
+  * Checks if either player has won the game and shows victory
+  * on baord if there is a winner.
+  *
+  * A win is when 3 squars line up in:
+  * 	- one row
+  * 		1,2,3
+  * 		4,5,6
+  * 		7,8,9
+  * 	- one column
+  * 		1,4,7
+  * 		2,5,8
+  * 		3,6,9
+  * 	- diagonal (top left or top right)
+  * 		1,5,9
+  * 		3,5,7
+  * 
+  * @return {Boolean} true means game is over, false means continue
+  */
+ function checkWin(state, player) {
+ 	for(let group of WINNING_GROUPS) {
+ 		if (group.every(index => state[index] == player)) {
+ 			return group;
+ 		}
+ 	}
+ }
+
+ function checkTie(state) {
+ 	let allFilled = state.every(entry => !!entry);
+ 	if (allFilled) {
+ 		board.className += " tie";
+ 		isGameInProgress = false;
+ 	}
+ }
